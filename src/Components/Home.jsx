039da9433ad01bc77c212e77771c67cf1bf613db
Shakawatullah-Tanin <mdshakawatullahtanin23@gmail.com>
2025-11-07 00:31:53 +0600

    add gradient for background

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router";
 
 const Home = () => {
   return (
     <div>
-      <nav className="bg-lime-200 p-4">
-        <div className="flex montserrat items-center justify-between">
-            <h1>Freelancer Hub</h1>
-          <ul className="flex items-center gap-4">
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink  to={"/"}>Add Task</NavLink>
-            <NavLink  to={"/"}>Browse Tasks</NavLink>
-            <NavLink  to={"/"}>My Posted Tasks</NavLink>
-          </ul> 
-          <div className="flex items-center  gap-4">
-            <button className="border p-2 ">Login</button>
-            <button  className="border p-2">SignUp</button>
-          </div>
-        </div>
-      </nav>
+    
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
+
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <nav className="nav-bg p-4">
+        <div className="flex montserrat items-center justify-between">
+          <h1 className="text-white">Freelancer Hub</h1>
+          <ul className="flex items-center gap-8">
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/"}>Add Task</NavLink>
+            <NavLink to={"/"}>Browse Tasks</NavLink>
+            <NavLink to={"/"}>My Posted Tasks</NavLink>
+          </ul>
+          <div className="flex items-center text-white  gap-4">
+            <button className=" p-2 ">Login</button>
+            <button className=" p-2">SignUp</button>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
