a81de781ab4a34d886ae4d88f998331f51403e1b
Shakawatullah-Tanin <mdshakawatullahtanin23@gmail.com>
2025-11-07 21:44:17 +0600

    add banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
+  const tech = ["Designer", "Developer", "Web", "IOS", "PHP", "Senior"];
+  return (
+    <div className="md:flex mx-4 items-center my-4 justify-between">
+      <div>
+        <h1 className="text-xl md:text-5xl font-bold">
+          Freelance Services For Your Business
+        </h1>
+        <p className="my-4">
+          Work with talented people at the most affordable price to get the most
+          out of your time and cost
+        </p>
         <div>
-            
+          <button className="px-4 py-2 btn">Get Start</button>
         </div>
-    );
+        <div className="my-4 ">
+          <h3 className="my-3">Populer Searches</h3>
+          <div className="grid grid-cols-4  gap-2">
+            {tech.map((item) => (
+              <p className="btn px-3 py-3">{item}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <img src="banner.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router";
+import { FiAlignLeft } from "react-icons/fi";
+
 
 
 const Navbar = () => {
@@ -7,14 +9,13 @@ const Navbar = () => {
     <div>
       <nav className="nav-bg p-4">
         <div className="flex montserrat items-center justify-between">
-          <h1 className="text-white">Freelancer Hub</h1>
-          <ul className="flex items-center gap-8">
-            <NavLink to={"/"}>Home</NavLink>
+          <h1 className="text-white text-lg flex items-center gap-2 justify-center"><span className="md:hidden"><FiAlignLeft size={25}></FiAlignLeft></span>Freelancer Hub</h1>
+          <ul className="hidden md:flex items-center effect  gap-8">
             <NavLink to={"/"}>Add Task</NavLink>
             <NavLink to={"/"}>Browse Tasks</NavLink>
             <NavLink to={"/"}>My Posted Tasks</NavLink>
           </ul>
-          <div className="flex items-center text-white  gap-4">
+          <div className="flex items-center text-white effect  gap-4">
             <button className=" p-2 ">Login</button>
             <button className=" p-2">SignUp</button>
           </div>
